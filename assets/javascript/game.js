86b0b1d8510e994ba877044425f3a4596c2dd5f1
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-03-19 21:50:42 -0500

    updated javascript variables back to original

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,70 @@
-//DEFINE VARIABLES
+// PLAYER VARIABLES & VALUES
+var thor = {
+    name: "THOR",
+    image: "assets/images/thor.png",
+    healthPoints: 180,
+    attackPower: 7,
+    counterAttackPower: 10,
+};
 
-var player
-var playerImage
-var playerHP
-var opponentHP
-var playerAP
+var ironMan = {
+    name: "IRON MAN",
+    image: "assets/images/ironman.png",
+    healthPoints: 140,
+    attackPower: 8,
+    counterAttackPower: 15,
+};
 
-//Game character object 
-function gameCharacter(name, hP, aP, cA, gImg) {
-	this.name = name;
-	this.healthPoints = hP;
-	this.attackPoints = aP;
-	this.counterAttack = cA;
-	this.gameImage = gImg; 
-	this.isFighter = false;
-}
+var blackWidow = {
+    name: "BLACK WIDOW",
+    image: "assets/images/black-widow.png",
+    healthPoints: 110,
+    attackPower: 18,
+    counterAttackPower: 6,
+};
 
-//Create new characters 
-var thor = new gameCharacter("THOR", 110, 15, 5, "assets/images/thor.png", false);
-var ironMan = new gameCharacter("IRON MAN", 120, 10, 8, "assets/images/ironman.png", false);
-var blackWidow = new gameCharacter("BLACK WIDOW", 160, 3, 18, "assets/images/black-widow.png");
-var hulk = new gameCharacter("HULK", 140, 7, 15, "assets/images/hulk.png", false);
-var captainAmerica = new gameCharacter("CAPTAIN AMERICA", 130, 6, 17, "assets/images/captain-america.png", false);
-//Add new characters to array 
-var availableCharacters = [thor, ironMan, blackWidow, hulk, captainAmerica];
+var hulk = {
+    name: "HULK",
+    image: "assets/images/hulk.png",
+    healthPoints: 200,
+    attackPower: 4,
+    counterAttackPower: 20,
+};
+
+var captainAmerica = {
+    name: "CAPTAIN AMERICA",
+    image: "assets/images/captain-america.png",
+    healthPoints: 120,
+    attackPower: 12,
+    counterAttackPower: 5,
+};
+
+// GLOBAL VARIABLES 
+var playersArray = [thor, ironMan, blackWidow, hulk, captainAmerica];
+var player;
+var opponent;
+var playerHP;
+var opponentHP;
+var playerHealthMeter;
+var opponentHealthMeter;
+var playerAP;
+var opponentAP;
+var noOpponent = true;
+var endGame = false;
+
 
 $(document).ready(function(){
+
 // DYNAMICALLY DISPLAY PLAYERS ON THE STARTING SCREEN
-for (var i=0; i<availableCharacters.length; i++) {
+for (var i=0; i<playersArray.length; i++) {
     // Create variables to create divs or tags to assign object 
     var healthPointTag = $("<p>");
-    healthPointTag.text(availableCharacters[i].healthPoints + " HP");
-    healthPointTag.attr("data-player", availableCharacters[i]);
+    healthPointTag.text(playersArray[i].healthPoints + " HP");
+    healthPointTag.attr("data-player", playersArray[i]);
     healthPointTag.addClass("card-text");
 
     var nameTag = $("<h3>");
-    nameTag.text(availableCharacters[i].name);
+    nameTag.text(playersArray[i].name);
     nameTag.addClass("card-title");
     // Append each new div or tag to each other for card display
     var cardBodyTag = $("<div>");
@@ -44,19 +73,19 @@ for (var i=0; i<availableCharacters.length; i++) {
    
     var imageTag = $("<img>");
     imageTag.addClass("card-img-top character");
-    imageTag.attr("src", availableCharacters[i].gameImage);
+    imageTag.attr("src", playersArray[i].image);
     imageTag.attr("alt", "game character" + [i]);
     imageTag.append(cardBodyTag);
 
     var cardTag = $("<div>");
     cardTag.addClass("card bg-transparent border-0 playerCard");
-    cardTag.attr("data-player", availableCharacters[i]);
+    cardTag.attr("data-player", playersArray[i]);
     cardTag.append(imageTag, cardBodyTag);
     
     // Append last div or tag to the existing div class from html
     $("#gameCharacters").append(cardTag);
 
-}
+};
 
 //-------------->
 
@@ -99,46 +128,48 @@ $("#attack").click(function() {
     if ($("#opponent-area").is(":empty")) {
 		$("#game-message").text("NO OPPONENT HERE!");
     }
-    else {
-        //Opponent loses points
-        opponentHP = opponentHP - playerAP;
-        //Player loses points from counter-attack
-        playerHP = playerHP - opponent.counterAttackPower;
-        $("#game-messsage").text(player.name + " HIT " + opponent.name + " FOR " + playerAP + " DAMAGE AND " + opponent.name + " HIT BACK WITH " + opponent.counterAttackPower + " DAMAGE.");
-    }
-    //Get rid of opponent if defeated
-    if(opponentHP <= 0) {
-		//empty fighting area
-		$("#opponent-area").empty();
-		//reset opponent
-		opponent = "";
-		// //reset health
-		// $("#opponent-health").text("");
-		//update onscreen message
-		$("#game-message").text("YOU DEFEATED YOUR OPPONENT!");
-		//check if anymore opponents left
-		if($("#opponent-area").is(":empty")) {
-		    //pause starting music
-            $("#fightsong").trigger("pause");
-            //start victory music
-            $("#victorysong").trigger("play");
-			//move player to win screen
-			$("#player-win").append(player);
-			//show win screen 
-			$(".win-screen").show();
-		} 
-    }
-    if (playerHP <= 0) {
-        //Player loses if health drops below zero
-            //pause starting music
-            $("#fightsong").trigger("pause");
-            //start losing music (need to get)
-            // $("#losersong").trigger("play");
-    }
-    //If attack button is pressed with no enemy, messagse shows to user (for after attack)
-	if($("opponent-area").is(":empty")) {
-		$("#game-message").text("No Opponent here!");
-	}
+
+//-------------->///NEED TO FIX // <------------------//
+//     else {
+//         //Opponent loses points
+//         opponentHP = opponentHP - playerAP;
+//         //Player loses points from counter-attack
+//         playerHP = playerHP - opponent.counterAttackPower;
+//         $("#game-messsage").text(player.name + " HIT " + opponent.name + " FOR " + playerAP + " DAMAGE AND " + opponent.name + " HIT BACK WITH " + opponent.counterAttackPower + " DAMAGE.");
+//     }
+//     //Get rid of opponent if defeated
+//     if(opponentHP <= 0) {
+// 		//empty fighting area
+// 		$("#opponent-area").empty();
+// 		//reset opponent
+// 		opponent = "";
+// 		// //reset health
+// 		// $("#opponent-health").text("");
+// 		//update onscreen message
+// 		$("#game-message").text("YOU DEFEATED YOUR OPPONENT!");
+// 		//check if anymore opponents left
+// 		if($("#opponent-area").is(":empty")) {
+// 		    //pause starting music
+//             $("#fightsong").trigger("pause");
+//             //start victory music
+//             $("#victorysong").trigger("play");
+// 			//move player to win screen
+// 			$("#player-win").append(player);
+// 			//show win screen 
+// 			$(".win-screen").show();
+// 		} 
+//     }
+//     if (playerHP <= 0) {
+//         //Player loses if health drops below zero
+//             //pause starting music
+//             $("#fightsong").trigger("pause");
+//             //start losing music (need to get)
+//             // $("#losersong").trigger("play");
+//     }
+//     //If attack button is pressed with no enemy, messagse shows to user (for after attack)
+// 	if($("opponent-area").is(":empty")) {
+// 		$("#game-message").text("No Opponent here!");
+// 	}
 });
 
 });
